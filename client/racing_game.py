e6831131d613a4f7aa13fe21d5e9a257f472b555
Gadi Ickowicz <gickowic@redhat.com>
2013-12-29 12:56:50 +0200

    Main game client class
    game client skeleton class

---
+++ client/racing_game.py
@@ -0,0 +1,62 @@
+author = 'gickowic'
+import pygame, sys
+
+
+class RacingGamingClient():
+
+    def init_pygame_resources(self):
+        pygame.init()
+        self.size = 640, 480
+        self.bg_color = 155, 0, 155
+        self.components = []
+
+    def update(self):
+        for component in self.components:
+            if component.__hasattr__('update'):
+                component.update()
+
+    def draw(self):
+        for component in self.components:
+            if component.__hasattr__('draw'):
+                component.draw()
+
+    def init(self):
+        for component in self.components:
+            if component.__hasattr__('init'):
+                if not component.initialized
+                component.init()
+
+    def main_game_loop(self):
+        screen = pygame.display.set_mode(self.size)
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.update()
+            self.draw()
+
+            screen.fill(self.bg_color)
+            pygame.display.flip()
+
+
+def main():
+    # TODO: Initialize pygame resources
+    game = RacingGamingClient()
+    game.init_pygame_resources()
+
+    # TODO: Initialize communications server
+
+    # TODO: construct objects (matrix + car)
+     ## create car
+
+     ## create matrix
+
+    # TODO: initialize objects
+    game.init()
+
+    # move to main game loop:
+    game.main_game_loop()
+
+
+if __name__ == '__main__':
+    main()
