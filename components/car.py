966b3449ade28cf7c47aa70181c392b2c0460602
Ohad Basan <obasan@redhat.com>
2013-12-29 16:52:14 +0200

    move car config to its own file

--- components/car.py
+++ components/car.py
@@ -1,5 +1,6 @@
-import matrix_config as config
+import matrix_config 
 import os
+import car_config
 
 class Car():
 
@@ -7,12 +8,12 @@ class Car():
     def __init__(self, id, x, y):
         self._location = (x, y)
         self._texture = os.path.join(
-            config.CAR_TEXTURE_FILES_DIR, config.CAR_TEXTURE_FILE
+            car_config.CAR_TEXTURE_FILES_DIR, car_config.CAR_TEXTURE_FILE
         ) + str(id)
 
     def draw(self, screen):
         screen.blit(
             self.texture,
-            self._location[1]*config.TILEHEIGHT,
-            self._location[0].config.TILEHEIGHT
+            self._location[1]*matrix_config.TILEHEIGHT,
+            self._location[0]*matrix_config.TILEHEIGHT
         )
---
+++ components/car_config.py
@@ -0,0 +1,2 @@
+CAR_TEXTURE_FILES_DIR = 'res/car_files'
+CAR_TEXTURE_FILE = 'cartexture'
