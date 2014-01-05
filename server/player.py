f4692e02e0981765243f620ca4762de183987c18
Nir Soffer <nsoffer@redhat.com>
2014-01-05 15:01:57 +0200

    Fix Player class
    - Initialize action correctly
    - Implement encoding

--- server/player.py
+++ server/player.py
@@ -6,8 +6,17 @@ MAX_LIVES = 3
 class Player(object):
     """
     """
-    def __init__(self, start_lane):
+    def __init__(self, name, start_lane):
+        self.name = name
         self.speed = 4  # y - location, between 0 to 8 on the board
         self.lane = start_lane  # x - between 0-3
         self.life = MAX_LIVES  # means how many blocks we can handle
-        self.action = matrix_config.ACTIONS[0]
+        self.action = matrix_config.ACTIONS['NONE']
+
+    def encode(self):
+        """ Return representation as native type the can be serialized """
+        return {'name': self.name,
+                'speed': self.speed,
+                'lane': self.lane,
+                'life': self.life}
+
