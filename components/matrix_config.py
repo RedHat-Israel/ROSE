877367dbce88dc9f2907b7bf008035d317d91304
Gadi Ickowicz <gickowic@redhat.com>
2014-01-05 11:16:58 +0200

    Actions and obstacles enum fix

--- components/matrix_config.py
+++ components/matrix_config.py
@@ -12,21 +12,19 @@ OBSTACLES_GLOB = 'client/res/obstacles/obstacle*.png'
 ROAD_GLOB = 'client/res/bg/bg*.png'
 
 # Obstacles
-EMPTY = 0
-OBSTACLES = {'WATER': 0,
-             'SNOW': 1,
-             'HOLE': 2,
-             'WALL': 3,
-             'OIL': 4,
-             'NAILS': 5,
-             'BUMPER': 6, }
+EMPTY = -1
+OBSTACLES = {'CRACK': 0,
+             'TRASH': 1,
+             'PENGIUN': 2,
+             'BIKE': 3,
+             'WATER': 4,
+             'BARRIER': 5}
 
-MOVE = {'FORWARD': 0,
-        'RIGHT': 1,
-        'LEFT': 2}
-
-ACTIONS = {'PICK': 0,
-           'JUMP': 1, }
+ACTIONS = {'RIGHT': 1,
+           'LEFT': 2,
+           'PICKUP': 3,
+           'JUMP': 4,
+           'BRAKE': 5}
 
 OBSTACLES_FOR_NEXT_ROW = OBSTACLES
 OBSTACLES_FOR_NEXT_ROW['EMPTY'] = EMPTY
