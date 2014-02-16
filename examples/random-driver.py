c9c41793302224228572642cba7794a6309caba2
Eli Mesika <emesika@redhat.com>
2014-02-16 15:55:05 +0200

    Rename random.py to random-dirver.py

---
+++ examples/random-driver.py
@@ -0,0 +1,14 @@
+"""
+Random driver
+
+This is a rather silly driver choosing the next action randomly. It is not a
+very good driver but the implementation is very elegant.
+"""
+import random
+from rtp.common import obstacles, actions
+
+server_address = "localhost"
+driver_name = "Random Driver"
+
+def drive(world):
+    return random.choice(actions.ALL)
