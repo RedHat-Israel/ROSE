a057fcac14d6d642c244fdbfdd2946b125074875
Yaniv Bronhaim <ybronhei@redhat.com>
2014-12-31 15:11:06 +0200

    Putting constants for all hordcoded values in dashboard.py
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- rtp/client/dashboard.py
+++ rtp/client/dashboard.py
@@ -7,6 +7,16 @@ import component
 
 class Dashboard(component.Component):
 
+    TIMER_FONT_COLOR = (153, 153, 153)
+    NAMES_FONT_COLOR = (153, 153, 153)
+    TIMER_FONT_SIZE = 70
+    NAMES_FONT_SIZE = 50
+    NAMES_START_POS = 50
+    NAMES_OFFSET_BETWEEN_SCORES = 530
+    TIMER_X_TWO_DIGEST_OFFSET = 27
+    TIMER_X_ONE_DIGEST_OFFSET = 15
+    TIMER_Y_OFFSET = 48
+
     def init(self):
         self.texture = pygame.image.load(config.dashboard_png)
         self.players = {}
@@ -17,19 +27,26 @@ class Dashboard(component.Component):
         self.players = players
 
     def draw(self, surface):
-        timer_font = pygame.font.SysFont(pygame.font.get_default_font(), 70)
-        timer = timer_font.render(self.timer, 1, (153, 153, 153))
+        timer_font = pygame.font.SysFont(pygame.font.get_default_font(),
+                                         Dashboard.TIMER_FONT_SIZE)
+        timer = timer_font.render(self.timer, 1, Dashboard.TIMER_FONT_COLOR)
         if int(self.timer) >= 10:
-            timer_x_pos = (config.windows_width / 2) - 27
+            timer_x_pos = (config.windows_width / 2) - \
+                    Dashboard.TIMER_X_TWO_DIGEST_OFFSET
         else:
-            timer_x_pos = (config.windows_width / 2) - 15
-        surface.blit(timer, (timer_x_pos, 48))
-        self.draw_name_and_score(surface, (50, config.player_name_and_score_pos))
+            timer_x_pos = (config.windows_width / 2) - \
+                    Dashboard.TIMER_X_ONE_DIGEST_OFFSET
+
+        surface.blit(timer, (timer_x_pos, Dashboard.TIMER_Y_OFFSET))
+        self.draw_name_and_score(surface, (Dashboard.NAMES_START_POS,
+                                 config.player_name_and_score_pos))
 
     def draw_name_and_score(self, surface, (x, y)):
-        font = pygame.font.SysFont(pygame.font.get_default_font(), 50)
+        font = pygame.font.SysFont(pygame.font.get_default_font(),
+                                   Dashboard.NAMES_FONT_SIZE)
         for player in self.players.values():
             name_and_score = "%(name)s : %(life)d" % player
-            text = font.render(str(name_and_score), 1, (153, 153, 153))
+            text = font.render(str(name_and_score), 1,
+                               Dashboard.NAMES_FONT_COLOR)
             surface.blit(text, (x, y))
-            x += 550
+            x += Dashboard.NAMES_OFFSET_BETWEEN_SCORES
