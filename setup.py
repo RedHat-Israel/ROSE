c27abbec11be1b47015612843952538b70e368d5
Rod Xavier Bondoc <rod.bondoc@kinesis.org>
2017-10-26 17:22:06 +1100

    Generate requirements.txt during sdist
    To support people that do not want to use pipenv, we generate a
    requirements.txt when building a tarball.

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
+import subprocess
 from distutils.core import setup
+from distutils.command import sdist
+
+
+class rose_sdist(sdist.sdist):
+    def run(self):
+        self.generate_files()
+        sdist.sdist.run(self)
+
+    def generate_files(self):
+        with open('requirements.txt', 'w') as f:
+            subprocess.check_call(['pipenv', 'lock', '--requirements'],
+                                  stdout=f)
+
 
 setup(name='rose-project',
       version='0.1',
@@ -9,4 +23,6 @@ setup(name='rose-project',
       author='Yaniv Bronhaim',
       author_email='ybronhei@redhat.com',
       url="https://github.com/emesika/RaananaTiraProject",
-      scripts=["rose-client", "rose-server", "rose-admin"],)
+      scripts=["rose-client", "rose-server", "rose-admin"],
+      data_files=[('requirements.txt', ['requirements.txt'])],
+      cmdclass={'sdist': rose_sdist})
