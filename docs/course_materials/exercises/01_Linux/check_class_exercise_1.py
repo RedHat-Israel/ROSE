1fab66353211fd84f8673ee688633630e924881d
Anna Dinaburg Vulikh <dinaburg@gmail.com>
2020-10-15 20:59:23 +0300

    Made the check_helper more general (#353)
    1. Made the 'home' a global variable in check_helper, this file would be used for all the exercises.
    2. Updated the 'get commands' function so it takes the right history file + looks at the last 10 days.
    3. Moved the functions for checking the exercise to the 'helper' file.

--- docs/course_materials/exercises/01_Linux/check_class_exercise_1.py
+++ docs/course_materials/exercises/01_Linux/check_class_exercise_1.py
@@ -1,28 +1,11 @@
-import os.path
 from pathlib import Path
 import check_helper
 
-home = str(Path.home())
 exercise_commands = {'mkdir': 'creating directories', 'tree': 'viewing the directory tree'}
-exercise_paths = [home + '/test/tmp']
+exercise_paths = [check_helper.HOME + '/test/tmp']
 
 def main():
     '''
     Running the tests and prompting the student according to his work.
     '''
-    test_result = ''
-    student_commands = check_helper.get_student_commands()
-    unused_commands = check_helper.find_unused_commands(student_commands, exercise_commands)
-    if len(unused_commands) > 0:
-        test_result += "It seems you missed some commands. Make sure you have used the following commands:\n"
-        for command in unused_commands:
-            test_result += "'" + command + "' for " + exercise_commands[command] + '.\n'
-    missing_paths = check_helper.created_directories(exercise_paths)
-    if len(missing_paths) > 0:
-        test_result += ("It seems that not all directories were created in the right place. "
-                    "Make sure you create the following:\n")
-        for cur_path in missing_paths:
-            test_result += cur_path + "\n"
-    if len(test_result) == 0:
-        test_result = 'Great work! You finished your exercise.'
-    print(test_result)
+    check_helper.test_exercise(exercise_commands, exercise_paths)
--- docs/course_materials/exercises/check_helper.py
+++ docs/course_materials/exercises/check_helper.py
@@ -1,20 +1,22 @@
-import os.path
+import os
 from pathlib import Path
+import datetime as dt
 
-home = str(Path.home())
+HOME = str(Path.home())
 
 def get_student_commands():
     '''
-    Checks stutent history file for key commands.
-    If the history is longer than 1000 commands, checks only 1000 last commands.
+    Checks stutent history file for key commands in the past 10 days.
     '''
+    shell_type = os.path.basename(os.environ["SHELL"])
+    past_date = str((dt.datetime.today() - dt.timedelta(days=10)).timestamp())  # used to get the epoch for 10 days back
+    hist_path = HOME + '/.' + shell_type + '_history'
+
     my_commands = []
-    hist_path = home + '/.bash_history'
     with open(hist_path, 'r') as rf:
         for line in rf:
-            my_commands.append(line[:-1])
-    if len(my_commands) > 1000:
-        my_commands = my_commands[-1000:]
+            if line[2:12] >= past_date:
+                my_commands.append(line[:-1])
     return my_commands
 
 def find_unused_commands(my_commands, exercise_commands):
@@ -33,3 +35,31 @@ def created_directories(exercise_paths):
         if not os.path.exists(cur_path):
             missing_paths.append(cur_path)
     return missing_paths
+
+def test_commands(exercise_commands):
+    student_commands = get_student_commands()
+    unused_commands = find_unused_commands(student_commands, exercise_commands)
+    test_result = ''
+    if len(unused_commands) > 0:
+        test_result += "It seems you missed some commands. Make sure you have used the following commands:\n"
+        for command in unused_commands:
+            test_result += "'" + command + "' for " + exercise_commands[command] + '.\n'
+    return test_result
+
+def test_paths(exercise_paths):
+    missing_paths = created_directories(exercise_paths)
+    test_result = ''
+    if len(missing_paths) > 0:
+        test_result += ("It seems that not all directories were created in the right place. "
+                    "Make sure you create the following:\n")
+        for cur_path in missing_paths:
+            test_result += cur_path + "\n"
+    return test_result
+
+
+def test_exercise(exercise_commands, exercise_paths):
+    test_result = test_commands(exercise_commands)
+    test_result += test_paths(exercise_paths)
+    if len(test_result) == 0:
+        test_result = 'Great work! You finished your exercise.'
+    print(test_result)
