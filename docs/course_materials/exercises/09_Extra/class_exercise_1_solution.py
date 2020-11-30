552d4a0fde29382d58f6437906204f47780731bd
Shani Leviim <sleviim@redhat.com>
2020-11-30 14:11:29 +0200

    Fix some phrasing on the 09_Extra section (#420)

--- docs/course_materials/exercises/09_Extra/class_exercise_1_solution.py
+++ docs/course_materials/exercises/09_Extra/class_exercise_1_solution.py
@@ -13,8 +13,8 @@ def joing_files(file_name1, file_name2, out_file_name):
         print('Reading or writing error with one of the files!')
 
 
-file_name_1 = input("Enter the first file name:  ")
-file_name_2 = input("Enter the secound file name:  ")
-file_name_3 = input("Enter the thired file name: ")
+file_name_1 = input("Enter the first file name: ")
+file_name_2 = input("Enter the second file name: ")
+file_name_3 = input("Enter the third file name: ")
 
 joing_files(file_name_1, file_name_2, file_name_3)
