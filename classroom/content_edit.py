9cdd57cf1aac8f30889c40a1149d0d7cb9f6eebf
Anna Dinaburg Vulikh <dinaburg@gmail.com>
2020-12-19 20:21:49 +0200

    updated commands

---
+++ classroom/content_edit.py
@@ -0,0 +1,37 @@
+import os
+from googleapiclient import errors
+
+"""
+Topics in classroom
+"""
+def print_topics(service):
+    # TODO: adapt the function for topics
+    # Call the Classroom API
+    results = service.courses().list(pageSize=10).execute()
+    courses = results.get('courses', [])
+
+    if not courses:
+        print('No courses found.')
+    else:
+        print('Courses:')
+        for course in courses:
+            print(f"Title: {course['name']}, ID: {course['id']}")
+
+def create_course(service, course_name):
+    course_data = {'name': course_name,
+                   'descriptionHeading': 'Welcome to ' + course_name,
+                   'description': ("We'll be learning about coding from a " +
+                                   "combination of lectures, course work, " +
+                                   "and home projects. Let's start!"),
+                   'ownerId': 'me',
+                   'courseState': 'PROVISIONED'}
+    course = service.courses().create(body=course_data).execute()
+    print(f"Course created: {course.get('name')} {course.get('id')}")
+    return course.get('id')
+
+def create_topic(service, title, course_id):
+    topic = {'name': title}
+    response = service.courses().topics().create(
+                                            courseId=course_id,
+                                            body=topic).execute()
+    print('Topic created: ', response['name'])
--- classroom/rose_class.py
+++ classroom/rose_class.py
@@ -1,65 +1,77 @@
 import argparse
 import logging
-import class_creator
+import course_creator
 import connect_service
+import content_edit
 
 
 def main():
     '''
     Getting user input and preforming corresponding actions.
     Available functions:
-    - Create class in classroom.
+    - Create course in classroom.
     - Update teacher list.
     - Update student list.
     '''
 
     logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(description='ROSE Classroom')
-    parser.add_argument('--create_class', '-c', dest='class_name',
-                        help='Creating a new class using given name. '
-                             'If not specified, '
-                             'class won\'t be created.')
-    parser.add_argument('--print_class', '-p', action="store_true",
-                        help='Printing existing classes.')
+    parser.add_argument('--course', action='store_true',
+                        help='A flag for course actions, stores True. '
+                             'Has to be followed by an action as --create. '
+                             'If not specified, will be False.')
+    parser.add_argument('--topic', action='store_true',
+                        help='A flag for topic actions, stores True. '
+                             'Has to be followed by an action as --create. '
+                             'If not specified, will be False.')
+    parser.add_argument('--create', '-c', dest='name',
+                        help='Creating a new instance using given name. '
+                             'If not specified, cannot be created. '
+                             'Follows an instance type as --course. '
+                             'For creating Topics, Assignments and more '
+                             'please specify the course/topic id using -i.')
+    parser.add_argument('--print', '-p', action="store_true",
+                        help='Printing existing instances.')
     parser.add_argument('--teacher_list', '-t', dest='teacher_list',
-                        help='Adding teachers using a list. '
-                             'Expects csv file.'
-                             'If class exists,'
-                             'please provide class ID using -i.')
+                        help='Adding teachers using a list, '
+                             'expects a csv file. '
+                             'If course exists, '
+                             'please provide course ID using -i.')
     parser.add_argument('--student_list', '-s', dest='student_list',
-                        help='Adding students using a list. '
-                             'Expects csv file.'
-                             'If class exists,'
-                             'please provide class ID using -i.')
-    parser.add_argument('--class_id', '-i',
-                        help='Specifies a class id for adding student '
-                             'lists or teacher lists.'
-                             'Use combined with -t and/or -s')
+                        help='Adding students using a list, '
+                             'expects csv file. '
+                             'If course exists, '
+                             'please provide course ID using -i.')
+    parser.add_argument('--id', '-i',
+                        help='Specifies an instance id. Can be used for '
+                             'adding student lists or teacher lists, adding '
+                             'topics, homework and more. '
+                             'Please specify the needed action. '
+                             'Use combined with instance type as --course.')
 
     args = parser.parse_args()
 
     '''Set up the service to google classroom'''
     service = connect_service.create_service()
 
-    if args.class_name is not None:
-        args.class_id = class_creator.create_course(service, args.class_name)
+    if args.course:
+        if args.name:
+            args.id = course_creator.create_course(service, args.name)
+            print(f'The id returned {args.id}')
+        elif args.print:
+            course_creator.print_courses(service)
+        elif not args.id:
+            print('Please use --help to inspect the possible actions.')
+        else:
+            if args.teacher_list:
+                course_creator.create_invitation(service, args, 'TEACHER')
 
-    if args.print_class:
-        class_creator.print_courses(service)
+            if args.student_list:
+                course_creator.create_invitation(service, args, 'STUDENT')
 
-    if args.class_id is None and not args.print_class:
-        print('Please use --help to inspect the possible actions.')
-    else:
-        if args.teacher_list is not None:
-            class_creator.create_invitation(service, args, 'TEACHER')
-
-        if args.student_list is not None:
-            class_creator.create_invitation(service, args, 'STUDENT')
-
-        no_list = args.student_list is None and args.teacher_list is None
-        if (no_list and not args.print_class):
-            print('Please use -s to update student list',
-                  'or/and -t to update teacher list.')
+            no_list = args.student_list is None and args.teacher_list is None
+            if (no_list):
+                print('Please use -h to check the available actions.')
 
 
 if __name__ == '__main__':
