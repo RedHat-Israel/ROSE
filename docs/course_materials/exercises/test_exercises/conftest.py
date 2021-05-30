391f14f0f65ae0d0fe6d41b427df21318641be6f
Anna Dinaburg Vulikh <dinaburg@gmail.com>
2021-05-30 21:31:09 +0300

    Updated the test homework method and docs

--- docs/course_materials/exercises/test_exercises/conftest.py
+++ docs/course_materials/exercises/test_exercises/conftest.py
@@ -9,11 +9,13 @@ The tests will check:
 import os
 import re
 import pytest
-from subprocess import PIPE, STDOUT, Popen, TimeoutExpired
+from subprocess import PIPE, Popen, TimeoutExpired
 import logging
 from rose_check import HOME
 
 LOGGER = logging.getLogger()
+# Allow next row for debug output.
+# LOGGER.setLevel('DEBUG')
 
 
 @pytest.fixture
@@ -24,8 +26,9 @@ def home_folder():
 class Test_helpers:
     def __init__(self):
         self.student_file = ''
-        self.expected_pycode = []
-        self.expected_stdout = []
+        self.expected_pycode = True
+        self.expected_stdout = True
+        self.tests_list = []
         self.input = []
         self.exact_answer = False
 
@@ -33,29 +36,37 @@ class Test_helpers:
         '''
         Runs all checks on a assignment
         1. If student answer file exists
-        2. If the writen code corresponds to the requirments
-        3. If the output corresponds to the requirments
+        2. If the written code corresponds to the requirements
+        3. If the output corresponds to the requirements
         '''
+        LOGGER.info(f'Started testing {self.student_file}:')
         self.test_file_exist()
 
-        # testing the code
-        if len(self.expected_pycode) > 0:
-            student_code = self.get_student_code()
-            # LOGGER.info(student_code)
-            self.test_answers(self.expected_pycode, student_code)
+        student_work = ''
 
-        # Testing the output
-        if len(self.expected_stdout) > 0:
+        # Runs the code and gets stdout
+        if self.expected_stdout:
+            LOGGER.info('Running the code...')
             if self.input:
-                for data, expected_stdout in zip(self.input,
-                                                 self.expected_stdout):
-                    student_stdout = self.run_cmd(data)
-                    self.test_answers([expected_stdout], [student_stdout],
-                                      word_pattern=self.exact_answer)
+                for data in self.input:
+                    student_work += self.run_cmd(data)
             else:
-                student_stdout = self.run_cmd()
-                self.test_answers(self.expected_stdout, student_stdout.strip(),
-                                  word_pattern=self.exact_answer)
+                student_work += self.run_cmd()
+                LOGGER.debug(f'run CMD, {student_work}')
+        
+        # Reads code file
+        if self.expected_pycode:
+            student_work += self.get_student_code()
+
+        # testing the code
+        LOGGER.info('Testing the code...')
+        LOGGER.debug(student_work)
+        test_code, message = self.test_answers(self.tests_list, student_work)
+        LOGGER.debug(message)
+        assert test_code, LOGGER.info('Good job, ' +
+                                      f'but needs some work:\n{message[:-1]}')
+        if message != '':
+            LOGGER.info(f'Better code can be achieved by:\n{message}')
 
     def test_file_exist(self):
         '''
@@ -82,14 +93,16 @@ class Test_helpers:
                       else None)
 
         p = Popen(['python', self.student_file], stdout=PIPE, stdin=PIPE,
-                  stderr=STDOUT)
+                  stderr=PIPE)
         try:
             stdout, stderr = p.communicate(input=input_data, timeout=20,
                                            **kwargs)
         except TimeoutExpired:
             p.kill()
             stdout, stderr = p.communicate()
-        assert p.returncode == 0, LOGGER.error(stderr)
+        if p.returncode != 0:
+            LOGGER.error('Failed while running' +
+                         f' the code. The error is:\n{stderr.decode()}')
 
         return stdout.decode('utf-8')
 
@@ -98,25 +111,49 @@ class Test_helpers:
         '''
         Compares students answers with expected ones.
         '''
+        better_code_only = True
         message = ''
         LOGGER.debug(f'expected: {expected_list}')
         for answer in expected_list:
             pattern = f'\\b{answer[0]}\\b' if word_pattern else answer[0]
             LOGGER.debug(f'pattern: {pattern}')
-            matched = False
             answers = answer_list if word_pattern else answer_list.splitlines()
-            for line in answers:
-                LOGGER.debug(f'line: |{line}|')
-                if re.search(pattern, line, re.MULTILINE):
-                    LOGGER.debug("MATCHED")
-                    matched = True
-                    break
+            LOGGER.debug(f'answers: {answers}')
+            matched = Test_helpers.test_answer_match(pattern, answers)
             if not matched:
-                message += '\n' + answer[1]
-        assert len(message) == 0, LOGGER.warning(message)
+                if isinstance(answer[1], list):
+                    test, better_message = Test_helpers.test_lesser_answers(
+                                                        answer[1],
+                                                        answers)
+                    message += better_message
+                    if not test:
+                        better_code_only = False
+                else:
+                    message += answer[1] + '\n'
+                    better_code_only = False
+        return better_code_only, message
 
     def set_student_file(self, file):
         self.student_file = os.path.join(str(HOME), file)
+    
+    @staticmethod
+    def test_answer_match(pattern, answers):
+        matched = False
+        for line in answers:
+            LOGGER.debug(f'line: |{line}|')
+            if re.search(pattern, line, re.MULTILINE):
+                LOGGER.debug("MATCHED")
+                matched = True
+                break
+        return matched
+
+    @staticmethod
+    def test_lesser_answers(expected_answer, answers):
+        LOGGER.debug("Checking lesser answers")
+        matched = Test_helpers.test_answer_match(expected_answer[0], answers)
+        if not matched:
+            return False, expected_answer[2]+'\n'
+        return True, expected_answer[1]+'\n'
 
 
 @pytest.fixture
--- docs/course_materials/exercises/test_exercises/how_to_add_python_tests.md
+++ docs/course_materials/exercises/test_exercises/how_to_add_python_tests.md
@@ -1,43 +1,54 @@
 # Adding test files for Python
 
 ## Setting up
+
 To add a test file:
+
 1. You should name it according to the `exercise/homework` theme.
 2. For every test function, add an appropriate decorator (aka `mark` in pytest). Use the same `mark('@pytest.mark.<topic>')` for tests you would like to run together. For example: `@pytest.mark.variables` for variables homework check.
 3. In order for `pytest` to recognize the new marks, you need to add them to the `pytest.ini` file.
 4. Each function in the test file should be named: `test_exercise_#` and should invoke `helpers` as input. For example: `def test_variables_1(helpers)`
 5. The `helpers` is defined by `pytest fixture` and creates a `Helpers class` that stores our check values and the functions for the testing.
 
-## Variables in the test files:
+## Variables in the test files
 
 Each test needs the following variables:
-1. **helpers.student_file**:  
-   Stores the path for student file for testing.
-   'set_student_file` function appends automatically the HOME directory or the directory we supplied to the name of the student file according to the homework instructions.
 
-2. **helpers.expected_pycode** (list):  
-   Should include code/text we expect in the student answer file and the error message to be displayed.  
+1. **helpers.student_file**(string):  
+   Stores the path for student file for testing.
+   `set_student_file` function appends automatically the HOME directory or the directory we supplied to the name of the student file according to the homework instructions.
+
+2. **helpers.expected_pycode** (boolean):
+   Initiated as True, if there is no code to check should be changed to False.
+
+3. **helpers.expected_stdout**(boolean):
+   Initiated as True, if there is no stdout to check should be changed to False.
+
+4. **helpers.tests_list**(list):
+   Should include code/text we expect in the student answer file/stdout and the error message to be displayed.
    The expected values should be supplied as string or in regex format.  
-   An example for checking code:  
-   ```
-   helpers.expected_pycode = [[r'^.*x *= *9.*', 'x should equal 9'], 
+   An example for the check:
+
+   ```Python
+   helpers.tests_list = [[r'^.*x *= *9.*', 'x should equal 9'], 
                               [r'^.*print\(.*z.*\).*',
                                  'please use the print function']]
    ```
 
-3. **helpers.input**(list):
+   You can also add a check for 'working but not optimal' solution by using the following:  
+
+ ```Python
+   helpers.tests_list = [[r"f'best \{solution\}",
+                               [r"other correct solution",
+                                'explanation for better solution',
+                                'error message']]]
+   ```
+
+5. **helpers.input**(list):
    Should include all the inputs we want to check.  
    For example: `helpers.input = [['10', '3'], ['5', '6']]`, will check 2 pairs of input.
 
-4. **helpers.expected_stdout**(list):  
-   Should include the output we expect to get and the error message to be displayed.  
-   An example for 4 printouts that can be prefixed with text:  
-   ```
-   helpers.expected_stdout = [[[r'.*30', r'.*7', r'.*' + str(10/3), r'.*1'],
-                               'the expected output is...']]
-   ```
-
-## Under the hood (using pytest):
+## Under the hood (using pytest)
 
 - rose_check.py calls pytest when `-t <mark>` is invoked
 - pytest.ini sets the parameters for the pytest run:
@@ -46,4 +57,4 @@ Each test needs the following variables:
     - -v for verbose, shows PASSED/FAILED
     - --no-header, --no-summary - hides those parts of outputs
   - markers - all our marks(decorators) should be registered here for pytest to recognize them.
-  - log_cle - specify which log to show on live log. Use `LOGGER.error(message)` to show output to the user (also available `LOGGER.info/warning`). 
+  - log_cle - specify which log to show on live log. Use `LOGGER.error(message)` to show output to the user (also available `LOGGER.info/warning`).
