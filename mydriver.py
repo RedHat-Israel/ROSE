2e24b9badfad3faacd0b47a1f224e6fbc02cd075
ArielP123 <ariel.petrovr@gmail.com>
2023-08-06 13:16:59 +0300

    basic drive, can't drive righet or left

--- mydriver.py
+++ mydriver.py
@@ -1,5 +1,27 @@
 from rose.common import obstacles, actions
+
 driver_name = "Car_6"
 
 def drive(world):
-    return actions.NONE
+    x = world.car.x
+    y = world.car.y
+
+    try:
+        obstacle = world.get((x, y - 1))
+    except IndexError:
+        pass
+        # change once look farther.
+
+
+
+    if obstacle == obstacles.NONE:
+        return actions.NONE
+    elif obstacle == obstacles.PENGUIN:
+        return actions.PICKUP
+    elif obstacle == obstacles.CRACK:
+        return actions.JUMP
+    elif obstacle == obstacles.WATER:
+        return actions.BRAKE
+    elif obstacle == obstacles.TRASH:
+        r
+
