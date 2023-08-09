976be2175c170439dfafa75da257e53e55daf0be
Phylisophyisawasteoftime <oshriyoungname1234@gmail.com>
2023-08-09 12:26:24 +0300

    working on new feature

--- mydriver.py
+++ mydriver.py
@@ -1,90 +1,89 @@
-import constans
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "Michael Schumacher"
+driver_name = "Emily"
 
-def bounds(world, x):
-    if x == 3 or x == 0:
-        return "Full L"
-    if x == 5 or x == 2:
-        return "Full R"
+scores = {obstacles.NONE: 0, obstacles.PENGUIN: 10, obstacles.WATER: 4, obstacles.CRACK: 5, obstacles.TRASH: -10, obstacles.BIKE: -10, obstacles.BARRIER: -10}
 
-def check_lane(world, x):
+ACTIONS_ob = {obstacles.NONE: False, obstacles.PENGUIN: actions.PICKUP, obstacles.WATER: actions.BRAKE, obstacles.CRACK: actions.JUMP, obstacles.TRASH: False, obstacles.BIKE: False, obstacles.BARRIER: False}
+
+
+def check_lane(x):
     if 0 <= x <= 2:
         return 1
     if 3 <= x <= 5:
         return 2
 
+def bounds_x(x):
+    if check_lane(x) == 1:
+        return [0,1,2]
+    if check_lane(x) == 2:
+        return [3,4,5]
 
-def generate_paths(world, x, y):
-    paths = []
-    x_paths = [x, x - 1, x + 1]
-    if bounds(world, x) == "Full L" and check_lane(world, x) == 1:
-        x_paths.remove(x - 1)
 
-    if bounds(world, x) == "Full R" and check_lane(world, x) == 2:
-        x_paths.remove(x + 1)
+def generate_paths(x, y):
+    paths = {}
+    x_paths = bounds_x(x)
+    for cx in x_paths:
+        paths[(cx, y - 2)] = []
+        if x - 1 < min(x_paths):
+            try:
+                del paths[(max(x_paths), y - 2)]
+            except:
+                pass
+        if x + 1 > max(x_paths):
+            try:
+                del paths[(min(x_paths), y - 2)]
+            except:
+                pass
 
-    for x in x_paths:
-        paths.append(((x, y - 1), (x, y - 2)))
+    for cx in x_paths:
+        for path in paths.keys():
+            paths[path].append((cx, y - 3))
+            if path[0] - 1 < min(x_paths):
+                try:
+                    paths[path].remove((max(x_paths), y - 3))
+                except:
+                    pass
+            if path[0] + 1 > max(x_paths):
+                try:
+                    paths[path].remove((min(x_paths), y - 3))
+                except:
+                    pass
     return paths
 
-def calc_score_for_path(world, path):
-    score = 0
-    x, y = path[-1]  # Get the current position of the car
+def calc_score_for_path(world, paths):
+    current_score = 0
+    highest_score = 0
+    x = world.car.x
+    x_paths = bounds_x(x)
+    best_path = x_paths[1]
+    for path in paths.keys():
+        obstacle_y_1 = world.get(path)
+        current_score = scores[obstacle_y_1]
+        for y2_path in paths[path][1::]:
+            obstacle_y_2 = world.get(y2_path)
+            current_score += scores[obstacle_y_2]
+        if current_score > highest_score:
+            highest_score = current_score
+            best_path = path[0]
+    if x == best_path:
+        action = actions.NONE
+    elif x < best_path and x + 1 in x_paths:
+        action = actions.RIGHT
+    elif x > best_path and x - 1 in x_paths:
+        action = actions.LEFT
 
-    try:
-        obstacle = world.get((x, y - 1))
-    except IndexError:
-        return actions.NONE  # Don't move if out of bounds
-
-    moves = []
-    for loc in path:
-        if obstacle == obstacles.NONE:
-            score += 0
-            moves.append(actions.NONE)
-        elif obstacle == obstacles.PENGUIN:
-            score += 10
-            moves.append(actions.PICKUP)
-        elif obstacle == obstacles.WATER:
-            score += 4
-            moves.append(actions.BRAKE)
-        elif obstacle == obstacles.CRACK:
-            score += 5
-            moves.append(actions.JUMP)
-        elif obstacle == obstacles.TRASH or obstacle == obstacles.BIKE or obstacle == obstacles.BARRIER:
-            score -= 10
-            moves.append(obstacles.BARRIER)
-    # Check for collisions with lane owner
-    if not (0 <= y < 10):
-        score -= 10
-
-    return score, moves
-
-def find_best_path(world, x, y):
-    paths = generate_paths(x, y)
-    best_score = float('-inf')
-    best_path = []
-    moves = []
-    for path in paths:
-        score, move = calc_score_for_path(world, path)
-        if score > best_score:
-            best_score = score
-            best_path = path
-            moves = move
-    return best_path, moves
+    print(best_path)
+    return action
 
 def drive(world):
     x = world.car.x
     y = world.car.y
-    best, moves = find_best_path(world, x, y)
-    # Check for obstacles at the next position
 
-    for loc in best:
-        for move in moves:
-            if move == obstacles.BARRIER:
-                if loc[0] <= x:
-                    return actions.LEFT
-                elif loc[0] >= x:
-                    return actions.RIGHT
-            return move
+    action = calc_score_for_path(world, generate_paths(x, y))
+    next_obstacle = world.get((x, y - 1))
+
+    if ACTIONS_ob[next_obstacle]:
+        return ACTIONS_ob[next_obstacle]
+    else:
+        return action
