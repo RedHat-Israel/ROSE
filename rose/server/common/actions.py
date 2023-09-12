f27c553893dab990f39284a63c583b9c253e848d
yzamir <kobi.zamir@gmail.com>
2023-09-12 13:00:44 +0300

    Add a common directory to the server application
    Signed-off-by: yzamir <kobi.zamir@gmail.com>

---
+++ rose/server/common/actions.py
@@ -0,0 +1,10 @@
+""" Driving actions """
+
+NONE = "none"  # NOQA
+RIGHT = "right"  # NOQA
+LEFT = "left"  # NOQA
+PICKUP = "pickup"  # NOQA
+JUMP = "jump"  # NOQA
+BRAKE = "brake"  # NOQA
+
+ALL = (NONE, RIGHT, LEFT, PICKUP, JUMP, BRAKE)
---
+++ rose/server/common/config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# Default Configuration
+drivers = []
+run = "stop"
+
+game_rate = 1.0
+game_duration = 60
+
+matrix_height = 9
+matrix_width = 6
+
+max_players = 2
+cells_per_player = 3
+
+score_move_forward = 10
+score_move_backward = -10
+score_pickup = 10
+score_jump = 5
+score_brake = 4
